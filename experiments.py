c276974eb346ec271d53851d7b861ab0a232ea22
Michael Bonnet <dolphinlink@gmail.com>
2022-02-17 20:19:34 -0700

    stuff

---
+++ experiments.py
@@ -0,0 +1,10 @@
+from arepl_dump import dump
+
+from pathlib import PurePosixPath
+
+path_string = '/mnt/PVE/SharePoint/ptnrMemblaze/Shared__Files/MemblazeFirmware/Weekly'
+
+ss = PurePosixPath(path_string)
+
+stringy = str(ss)
+
